ab4e28b3dc8e935fddc66be049aaaefc218e4e3e
mugeni02 <mugenir7@gmail.com>
2023-11-27 10:30:57 -0800

    More pointers, strings, and arrays

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* *_memcpy - that copies memory area
+* @dest: variable 1
+* @src: variable 2
+* @n: bytes
+* Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* *_strpbrk - that searches a string for any of a set of bytes
+* @s: variable pointer
+* @accept: variable pointer
+* Return: string
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	int i, j;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+		if (s[i] == accept[j])
+			{
+			s = s + i;
+			return (s);
+			}
+		}
+	}
+	s = 0;
+	return (s);
+}
