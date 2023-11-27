65c034c01fdcd2400951cc6e0798a9b70d4fb4eb
mugeni02 <mugenir7@gmail.com>
2023-11-27 10:46:40 -0800

    correct 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,26 +1,13 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
-* print_diagsums - prints the sum of the two diagonals of a square matrix
-* @a: variable pointer 1
-* @size: variable int
-* Return:
-*/
-
-void print_diagsums(int *a, int size)
+ * set_string - Set the value of a pointer to a char.
+ * @s: pointer to the char pointer to change
+ * @to: pointer to change the char pointer to
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
 {
-	int i, j;
-	int sumx = 0;
-	int sumy = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		sumx = sumx + a[(size + 1) * i];
-	}
-	for (j = 0; j < size; j++)
-	{
-		sumy = sumy + a[(size - 1) * (j + 1)];
-	}
-
-	printf("%d, %d\n", sumx, sumy);
+	*s = to;
 }
