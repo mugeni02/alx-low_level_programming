103fe8958f5f8a795a3dfddf63249043601e0299
mugeni02 <mugenir7@gmail.com>
2024-01-15 12:15:57 -0800

    correction

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
-  * _pow_recursion - function name
-  * @x: parameter 1
-  * @y: parameter 2
-  * Return: value of a given number passed to the function
-  */
-
-
+ * _pow_recursion - return the power of a natural number.
+ * @x: natural number
+ * @y: power
+ *
+ * Return: the power.
+ */
 int _pow_recursion(int x, int y)
 {
-
-if (y < 0)
-        return (-1);
-
-else if (y == 0)
-        return (1);
-
-else
-        return (x * _pow_recursion(x, (y - 1)));
-
-return (0);
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, (y - 1)));
+	}
 }
